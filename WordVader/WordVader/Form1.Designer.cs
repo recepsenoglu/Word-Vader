c4540f509ac97c153ce66aec57b3cda76f4b87c0
Recep Oğuzhan Şenoğlu <83721234+recepsenoglu@users.noreply.github.com>
2021-06-07 16:39:10 +0300

    WordWader

---
+++ WordVader/WordVader/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WordVader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtgiris = new System.Windows.Forms.RichTextBox();
+            this.txtcikis = new System.Windows.Forms.RichTextBox();
+            this.btnok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtgiris
+            // 
+            this.txtgiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtgiris.Location = new System.Drawing.Point(24, 12);
+            this.txtgiris.Name = "txtgiris";
+            this.txtgiris.Size = new System.Drawing.Size(404, 291);
+            this.txtgiris.TabIndex = 0;
+            this.txtgiris.Text = "";
+            this.txtgiris.TextChanged += new System.EventHandler(this.txtgiris_TextChanged);
+            // 
+            // txtcikis
+            // 
+            this.txtcikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtcikis.Location = new System.Drawing.Point(548, 12);
+            this.txtcikis.Name = "txtcikis";
+            this.txtcikis.ReadOnly = true;
+            this.txtcikis.Size = new System.Drawing.Size(404, 291);
+            this.txtcikis.TabIndex = 2;
+            this.txtcikis.Text = "";
+            // 
+            // btnok
+            // 
+            this.btnok.Enabled = false;
+            this.btnok.Location = new System.Drawing.Point(447, 12);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(75, 291);
+            this.btnok.TabIndex = 1;
+            this.btnok.Text = "OK";
+            this.btnok.UseVisualStyleBackColor = true;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 324);
+            this.Controls.Add(this.btnok);
+            this.Controls.Add(this.txtcikis);
+            this.Controls.Add(this.txtgiris);
+            this.Name = "Form1";
+            this.Text = "it\'s over anakin";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtgiris;
+        private System.Windows.Forms.RichTextBox txtcikis;
+        private System.Windows.Forms.Button btnok;
+    }
+}
+
---
+++ WordVader/WordVader/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordVader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList words = new ArrayList();
+        string temp_word;
+        string first_letter;
+        string last_letter;
+        string mid_letter;
+        string shuffled_mid;
+        private void txtgiris_TextChanged(object sender, EventArgs e)
+        {
+            if (txtgiris.Text != "") btnok.Enabled = true;
+            else btnok.Enabled = false;
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            txtcikis.Text = "";
+            words.Clear();
+            
+            txtgiris.Text += " ";
+            Method(txtgiris.Text);
+
+        }
+
+        void Method(string txt)
+        {
+            foreach(char letter in txt)
+            {
+                if (letter.ToString() != " " && letter.ToString() != "." && letter.ToString() != ",")
+                {
+                    temp_word += letter.ToString();
+                }
+                else
+                {
+                    if(temp_word != "") words.Add(temp_word);
+                    temp_word = "";
+                }
+            }
+            
+            foreach(string word in words)
+            {
+                int word_length = word.Length;
+                if (word_length < 4) txtcikis.Text += word + " ";
+                else
+                {
+                    first_letter = word.Substring(0, 1);
+                    last_letter = word.Substring(word.Length - 1);
+                    mid_letter = word.Substring(1, word.Length - 2);
+                    int mid_lenght = mid_letter.Length;
+                    Random random = new Random();
+                    int random_index = 0;
+                    for (int i = 0; i < mid_lenght; i++)
+                    {
+                        random_index = random.Next(0, mid_letter.Length);
+                        shuffled_mid += mid_letter[random_index];
+                        mid_letter = mid_letter.Remove(random_index,1);
+                    }
+                    string new_word = first_letter + shuffled_mid + last_letter;
+                    txtcikis.Text += new_word + " ";
+                    shuffled_mid = "";
+                }
+            }
+
+        }
+    }
+}
